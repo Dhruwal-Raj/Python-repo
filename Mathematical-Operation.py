7c1b096cd4d9e6aacefe5ca3535f743ff996df56
Dhruwal-Raj <araj2369@gmail.com>
2025-08-06 19:39:59 +0530

    Add files via upload

---
+++ Mathematical-Operation.py
@@ -0,0 +1,27 @@
+# Mathematical Operation
+
+# Addition
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+add=num1+num2
+
+print("Addition: ",add)
+
+# Subtraction
+
+minus=num1-num2
+
+print("Subtraction: ",minus)
+
+# Multiplication
+
+multiply=num1*num2
+
+print("Multiplication: ",multiply)
+
+# Division
+
+divide=num1/num2
+
+print("Division: ",divide)
