c12b0f92bffca783ae7c8c0b0820de909ea13871
Dhruwal-Raj <araj2369@gmail.com>
2025-08-08 21:11:20 +0530

    Add files via upload

---
+++ Even or Odd.py
@@ -0,0 +1,7 @@
+#Even or Odd
+
+n=int(input("Enter a number :"))
+if n%2==0:
+    print(n,"is an even number")
+else:
+    print(n,"is an odd number")
---
+++ sum of integers.py
@@ -0,0 +1,5 @@
+#For loop
+n=50
+#total=n*(n+1)//2
+total=sum(range(1,51)) #1 to 50
+print("The sum of number from 1 to 50 is : ",total)
