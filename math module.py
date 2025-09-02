7cfc094599c2b26de5b90e4768a4a83aad7f143b
Dhruwal-Raj <araj2369@gmail.com>
2025-09-02 16:30:19 +0530

    Update math module.py

--- math module.py
+++ math module.py
@@ -1,14 +1,15 @@
 import math
 
-# User se number input lena
+# Taking inpur from user.
 num = float(input("Enter a number: "))
 
-# Calculations using math module
+# Calculations using math module.
 square_root = math.sqrt(num)
 natural_log = math.log(num)       # ln(num)
 sine_value = math.sin(num)        # sine in radians
 
-# Results display karna
+# For display result.
 print("Square Root:", square_root)
 print("Logarithm:", natural_log)
 print("Sine:", sine_value)
+
